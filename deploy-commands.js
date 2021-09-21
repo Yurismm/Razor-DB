da271282a52254f97e34b677fdff3051444dff23
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2021-09-21 14:28:56 +0100

    changes 4

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,14 +11,6 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-
-const commands = [
-        new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-        new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-        new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-    ]
-    .map(command => command.toJSON());
-
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async() => {
