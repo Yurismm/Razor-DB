fcccb7440cea605a4f14040b110375aa5605e6c7
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2021-09-21 12:11:34 +0100

    bot

--- bot.js
+++ bot.js
@@ -7,5 +7,22 @@ client.once("ready", () => {
     console.log("Razor-DB is online")
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        await interaction.reply('Server info.');
+    } else if (commandName === 'user') {
+        await interaction.reply("Your tag:" + interaction.user.tag + "\nYour id:" + interaction.user.id);
+    }
+});
+
+
+
+
 
 client.login(token)
