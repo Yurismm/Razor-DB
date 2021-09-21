351391dd8d262ab97ca7c68329fbf5873689206c
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2021-09-21 11:47:59 +0100

    added client file

---
+++ bot.js
@@ -0,0 +1,11 @@
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+    console.log("Razor-DB is online")
+});
+
+
+client.login(token)
