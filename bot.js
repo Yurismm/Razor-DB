003f66571c69c577843cf73ea3d186e6f84f37bf
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2021-12-06 20:56:01 +0000

    a lot of weird changes, the help command is hard and i might just do it manually for now

--- bot.js
+++ bot.js
@@ -6,19 +6,48 @@ const { token } = require('./config.json');
 const { prefix } = require("./config.json");
 const fs = require('fs');
 
-client.commands = new Discord.Collection();
-
 client.once("ready", () => {
     console.log("Razor-DB is online")
     client.user.setActivity('cheries weirdness', { type: 'COMPETING' });
 });
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+client.commands = new Discord.Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
 }
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName) ||
+        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    if (command.permissions) {
+        const authorPerms = message.channel.permissionsFor(message.author);
+        if (!authorPerms || !authorPerms.has(command.permissions)) {
+            return message.reply('You can not do this!');
+        }
+    }
+});
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
---
+++ commands/util/help.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+
+const { prefix } = require('../../config.json');
+
+module.exports = {
+    name: 'help',
+    description: 'List all of my commands or info about a specific command.',
+    aliases: ['commands'],
+    usage: '[command name]',
+    cooldown: 5,
+    execute(message, args) {
+        const data = [];
+        const { commands } = message.client;
+
+        if (!args.length) {
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle('Here\'s a list of all my commands:')
+                .addFields({ name: '**Commands:**', value: commands.map(command => command.name).join(', ') }, { name: '\u200B', value: `For help on a specific command send: \`${prefix}help [command name]\`` })
+
+            return message.author.send({ embeds: [embed] })
+                .then(() => {
+                    if (message.channel.type === 'dm') return;
+                    message.channel.send(`☑️ I've sent you a DM with all of my commands, ${message.author}.`);
+                })
+                .catch(error => {
+                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                    message.channel.send(`Seems like I can't DM you, ${message.author}. Do you have DMs disabled?`);
+                });
+        }
+        const name = args[0].toLowerCase();
+        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+        if (!command) {
+            return message.reply('that\'s not a valid command!');
+        }
+
+        data.push(`**Name:** ${command.name}`);
+
+        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description) data.push(`**Description:** ${command.description}`);
+        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+        message.channel.send(data, { split: true });
+    },
+};
