f005e3fcd580e3c166e48056c1d3856acf377c00
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2022-07-21 13:08:47 +0100

    unconfirmed push

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const genshindb = require('genshin-db');
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"], partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"] });
 const { token } = require('./config.json');
 const { prefix } = require("./config.json");
 const fs = require('fs');
+
 
 client.once("ready", () => {
     console.log("Razor-DB is online")
---
+++ commands/util/rolegive.js
@@ -0,0 +1,23 @@
+const { DiscordAPIError } = require("discord.js");
+const Discord = require("discord.js");
+
+
+
+
+module.exports = {
+    name: "rolvegive",
+    description: "gives role",
+    execute(message, args) {
+
+        let role = message.guild.roles.cache.find(r => r.id === "901522952338677801");
+
+
+        message.guild.members.filter(m => !m.user.bot).forEach(member => member.addRole(role))
+        const embed1 = new Discord.MessageEmbed()
+            .setColor("#A020F0")
+            .setTitle("cmd push")
+            .setURL("check cmdline")
+            .setFooter("Razor-DB")
+        message.channel.send({ embeds: [embed1] })
+    }
+}
